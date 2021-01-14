34f05f5aa2b9bec9bb04ff380beb58593f6def24
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-01-13 22:20:21 -0600

    font and icon links

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
 
-    var APIKey = "fccfd79af0d76ce3841c8154e44fca10"
-    var queryURL = 
+    var APIKey = "fccfd79af0d76ce3841c8154e44fca10";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + APIKey;
+    
+
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+        
+    
+        console.log(this);
+
+
 
 
 }
