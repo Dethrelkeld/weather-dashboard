42f04737af3feab01af7b09528d889fda0a728ec
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-01-14 02:03:13 -0600

    started functions

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,41 @@
 $(document).ready(function () {
 
+
+
     var APIKey = "ffccfd79af0d76ce3841c8154e44fca10";
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + stateCode + "&appid=" + APIKey;
 
     var cityName = "arlington";
-    var stateCode = "TX"
+    var stateCode = "TX";
 
+    $("#buttonSrch").on("click", function (event) {
+        event.preventDefault();
 
-    
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-
-
-
-
+        var cityInput = $("#cityInput").val().trim();
 
 
     });
+
+    // call the API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+
+    });
+
+    // display the forecast
+    function renderDays() {
+
+    };
+
+    // display the current weather 
+    function renderCurrent() {
+
+    };
+
+
 
 })
