afcca0390d5f38ecc7f641d35d454a7b076dcd08
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-01-13 23:26:56 -0600

    minor styling

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
 
     var APIKey = "ffccfd79af0d76ce3841c8154e44fca10";
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + stateCode + "&appid=" + APIKey;
+
+    var cityName = "arlington";
+    var stateCode = "TX"
 
 
     
